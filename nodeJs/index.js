ed0aa9e54a5195e28239bd3b3f55400e2f0a8b09
Kolhesuraj <surajkolhe214@gmail.com>
2022-11-08 15:04:18 +0530

    8/nov

--- nodeJs/index.js
+++ nodeJs/index.js
@@ -1,6 +1,16 @@
-const http = require('http');
+// const http = require('http');
+const express = require('express');
+const app = express();
+app.get('', (req,resp) => {
+    resp.send('This is home page');
+});
 
-http.createServer((req, resp) => {
-    resp.write("response");
-    resp.end();
-}).listen(4000);
+app.get('/about', (req,resp) => {
+    resp.send('This is about page');
+})
+
+// http.createServer((req, resp) => {
+//     resp.write("response");
+//     resp.end();
+// }).listen(4000);
+app.listen(5000);
